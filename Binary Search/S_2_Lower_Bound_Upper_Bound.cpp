89624dc5c6fc95869a3551c81df079b578c59a2b
Srinath - ASUS <srinathdivate123@gmail.com>
2024-02-15 10:36:55 +0530

    Back to Binary Search

--- Binary Search/S_2_Lower_Bound_Upper_Bound.cpp
+++ Binary Search/S_2_Lower_Bound_Upper_Bound.cpp
@@ -1,11 +1,9 @@
 // Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
 
-// The lower bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than or equal to a given target i.e. x.
-
-// The lower bound is the smallest index, ind, where arr[ind] >= x. But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array.
 
 #include <bits/stdc++.h>
 using namespace std;
+// The lower bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than or equal to a given target i.e. x. The lower bound is the smallest index, ind, where arr[ind] >= x. But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array.
 
 // In C++ we have a in-built function for this.
 // int lb = lower_bound(arr.begin(), arr.end(), target);
@@ -18,15 +16,27 @@ int lowerBound(vector<int> arr, int n, int target)
     {
         int mid = low + (high - low) / 2;
         if (arr[mid] >= target)
-            ans = mid, high = mid - 1; // Look for more smaller index on left
+            ans = mid, high = mid - 1; // You found an element that is greater than or equal to target. But look for more smaller index on left if it exists
         else
             low = mid + 1; // Look for right
     }
     return ans;
 }
+
+// The upper bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than the given key i.e. x. The upper bound is the smallest index, ind, where arr[ind] > x. But if any such index is not found, the upper bound algorithm returns n i.e. size of the given array. The main difference between the lower and upper bound is in the condition. For the lower bound the condition was arr[ind] >= x and here, in the case of the upper bound, it is arr[ind] > x.
 int upperBound(vector<int> arr, int n, int target)
 {
-    
+    int low = 0, high = n - 1, ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] > target)
+            ans = mid, high = mid - 1; // You found an element that is greater than target. But look for more smaller index on left if it exists
+        else
+            low = mid + 1; // Look on the right
+    }
+    return ans;
 }
 int main()
 {
