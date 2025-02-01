7164a40d2c9558003da38ba1c145487d0d4602c1
Srinath - ASUS <srinathdivate123@gmail.com>
2025-02-01 12:31:12 +0530

    S_2_Longest_Substr_Without_Repeating_Chars

--- Sliding_Win_Two_Pointer/S_1_Max_Points_You_Can_Obtain_From_Cards.cpp
+++ Sliding_Win_Two_Pointer/S_1_Max_Points_You_Can_Obtain_From_Cards.cpp
@@ -1,4 +1,14 @@
-// You can take a card from front, back or both.but they should be consecutive
+/* 
+There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+You can take a card from front, back or both, but they should be consecutive
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
---
+++ Sliding_Win_Two_Pointer/S_2_Longest_Substr_Without_Repeating_Chars.cpp
@@ -0,0 +1,15 @@
+// Given a string s, find the length of the longest substring  without repeating characters.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthofLongestSubstring(string s)
+{
+
+}
+
+int main()
+{
+    string str = "takeUforward";
+    cout << lengthofLongestSubstring(str);
+}
