15d0cec6341b185c2b31945d4c56c10c33efc482
Srinath - ASUS <srinathdivate123@gmail.com>
2025-02-15 13:03:40 +0530

    S_13_Fractional_Knapsack

---
+++ Greedy_Algorithms/S_13_Fractional_Knapsack.cpp
@@ -0,0 +1,48 @@
+/*
+The weight of N items and their corresponding values are given. We have to put these items in a knapsack of weight W such that the total value obtained is maximized.
+
+Note: We can either take the item as a whole or break it into smaller units.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item
+{
+    int value;
+    int weight;
+};
+
+bool static comp(Item a, Item b)
+{
+    double r1 = (double)a.value / (double)a.weight;
+    double r2 = (double)b.value / (double)b.weight;
+    return r1 > r2;
+}
+
+double fractionalKnapsack(int W, Item arr[], int n)
+{
+    sort(arr, arr + n, comp); // Sort by the maximum value per weight which you get (value/weight)
+
+    double totalvalue = 0.0;
+
+    for (int i = 0; i < n; i++)
+        if (arr[i].weight <= W)
+        {
+            totalvalue += arr[i].value;
+            W = W - arr[i].weight;
+        }
+        else
+        {
+            totalvalue += (arr[i].value / (double)arr[i].weight) * (double)W;
+            break;
+        }
+    return totalvalue;
+}
+
+int main()
+{
+    int n = 3, weight = 50;
+    Item arr[n] = {{100, 20}, {60, 10}, {120, 30}};
+    cout << setprecision(2) << fixed << fractionalKnapsack(weight, arr, n);
+}
