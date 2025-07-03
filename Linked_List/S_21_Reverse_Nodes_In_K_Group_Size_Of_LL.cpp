428062f9644054e0595ab8167d91243718cca4be
Srinath - ASUS <srinathdivate123@gmail.com>
2025-07-03 22:02:04 +0530

    S_21_Reverse_Nodes_In_K_Group_Size_Of_LL

--- Linked_List/S_21_Reverse_Nodes_In_K_Group_Size_Of_LL.cpp
+++ Linked_List/S_21_Reverse_Nodes_In_K_Group_Size_Of_LL.cpp
@@ -5,3 +5,108 @@
 // k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
 
 // You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *reverseLinkedList(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    Node *temp = head;
+    Node *prev = NULL;
+    Node *front = NULL;
+
+    while (temp != NULL)
+    {
+        front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
+
+Node *getKthNode(Node *temp, int k)
+{
+    while (--k && temp != NULL)
+        temp = temp->next;
+    return temp;
+}
+
+Node *kReverse(Node *head, int k)
+{
+    Node *temp = head;
+    Node *prevLast = NULL;
+    while (temp != NULL)
+    {
+        Node *kthNode = getKthNode(temp, k);
+        if (kthNode == NULL)
+        {
+            if (prevLast)
+                prevLast->next = temp;
+            break;
+        }
+        Node *nextNode = kthNode->next;
+        kthNode->next = NULL;
+        reverseLinkedList(temp);
+        if (temp == head)
+        {
+            head = kthNode;
+        }
+        else
+        {
+            prevLast->next = kthNode;
+        }
+        prevLast = temp;
+        temp = nextNode;
+    }
+    return head;
+}
+
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(5);
+    head->next = new Node(4);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(7);
+    head->next->next->next->next = new Node(9);
+    head->next->next->next->next->next = new Node(2);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    head = kReverse(head, 4);
+
+    cout << "Reversed Linked List: ";
+    printLinkedList(head);
+    return 0;
+}
