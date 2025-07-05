3834580409cb9ace2aa1aa74cf87281f9bbaa56b
srinathdivate123 <srinathdivate123@gmail.com>
2025-07-05 23:28:29 +0530

    S_23_Merge_Two_Sorted_LL

---
+++ Linked_List/S_23_Merge_Two_Sorted_LL.cpp
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *sortTwoLinkedLists(Node *list1, Node *list2)
+{
+    // Create a dummy node to serve as the head of the merged list
+    Node *dummyNode = new Node(-1);
+    Node *temp = dummyNode;
+
+    while (list1 != nullptr && list2 != nullptr)
+    {
+        if (list1->data <= list2->data)
+        {
+            temp->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        temp = temp->next;
+    }
+
+    // If any list still has remaining elements, append them to the merged list
+    if (list1 != nullptr)
+        temp->next = list1;
+    else
+        temp->next = list2;
+    return dummyNode->next;
+}
+
+// Function to print the linked list
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        // Print the data of the current node
+        cout << temp->data << " ";
+        // Move to the next node
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *list1 = new Node(1);
+    list1->next = new Node(3);
+    list1->next->next = new Node(5);
+
+    Node *list2 = new Node(2);
+    list2->next = new Node(4);
+    list2->next->next = new Node(6);
+
+    cout << "First sorted linked list: ";
+    printLinkedList(list1);
+
+    cout << "Second sorted linked list: ";
+    printLinkedList(list2);
+
+    Node *mergedList = sortTwoLinkedLists(list1, list2);
+
+    cout << "Merged sorted linked list: ";
+    printLinkedList(mergedList);
+
+    return 0;
+}
