03fe6693cedcbb895e163bedc707c9ea8a06e35c
Srinath - ASUS <srinathdivate123@gmail.com>
2025-12-07 17:31:45 +0530

    S_G10_Rotten_Oranges

--- Graphs/S_G10_Rotten_Oranges.cpp
+++ Graphs/S_G10_Rotten_Oranges.cpp
@@ -1,8 +1,14 @@
-// Problem Statement: Given a grid of dimension N x M where each cell in the grid can have values 0, 1, or 2 which has the following meaning:
-// 0: Empty cell
-// 1: Cells have fresh oranges
-// 2: Cells have rotten oranges
-// We have to determine what is the minimum time required to rot all oranges. A rotten orange at index [i,j] can rot other fresh oranges at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) all simultaneously in unit time.
+// https://takeuforward.org/data-structure/rotten-oranges-min-time-to-rot-all-oranges-bfs/
+// https://leetcode.com/problems/rotting-oranges/description/
+
+// You are given an m x n grid where each cell can have one of three values:
+
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,12 +17,18 @@ class Solution
 public:
     int orangesRotting(vector<vector<int>> &grid)
     {
+        if (grid.empty())
+            return 0;
+
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<pair<int, int>, int>> q; // {{row, col}, time} --> Storing the row, col, time of the rotting oranges
-        vector<vector<int>> vis;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
         int cntFresh = 0;
+
         for (int i = 0; i < n; i++)
+        {
             for (int j = 0; j < m; j++)
             {
                 // Iterate once through the entire matrix and push all the rotten oranges into the queue!
@@ -25,16 +37,17 @@ public:
                     q.push({{i, j}, 0});
                     vis[i][j] = 2;
                 }
-                else
-                    vis[i][j] = 0;
                 // Count number of fresh oranges
                 if (grid[i][j] == 1)
                     cntFresh++;
             }
+        }
+
         int time = 0;
-        int delRow[] = {-1, 0, +1, 0}; // This is the difference of row for 4 neighbours
-        int delCol[] = {0, +1, 0, -1}; // This is the difference of col for 4 neighbours
+        int delRow[] = {-1, 0, 1, 0}; // This is the difference of row for 4 neighbours
+        int delCol[] = {0, 1, 0, -1}; // This is the difference of col for 4 neighbours
         int cnt = 0;
+
         while (!q.empty())
         {
             int r = q.front().first.first;
@@ -50,13 +63,16 @@ public:
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1)
                 {
                     q.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 1;
+                    vis[nrow][ncol] = 2;
                     cnt++;
                 }
             }
         }
-        if (cnt != cntFresh) // If we did not push all the fresh oranges into the queue, we will not end up rotting all the oranges, hence return -1
+        if (cnt != cntFresh)
+        // If we did not push all the fresh oranges into the queue, we will not end up rotting all the oranges, hence return -1
+        {
             return -1;
+        }
         return time;
     }
 };
