60f4dcc07540680045a5cda1de8509ec54f9b008
Srinath - ASUS <srinathdivate123@gmail.com>
2025-12-12 21:36:49 +0530

    S_G17_Bipartite_Graph

---
+++ Graphs/S_G17_Bipartite_Graph.cpp
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/is-graph-bipartite/description/
+
+// TC = N + 2E
+// SC = 2N : Queue, colour array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool colorGraph(int start, int edges, vector<vector<int>> graph, vector<int> colour)
+    {
+        queue<int> q;
+        q.push(start);
+        colour[start] = 0;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbour : graph[node])
+            {
+                // If the adjacent node is not yet coloured
+                // You will give the opposite colour of the current node to it
+                if (colour[neighbour] == -1)
+                {
+                    colour[neighbour] = !colour[node];
+                    q.push(neighbour);
+                }
+                // If the adjacent node has the same colour as the current node
+                // Then someone else had coloured it previously
+                else if (colour[neighbour] == colour[node])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int m = graph.size();
+        vector<int> colour(m, -1);
+        for (int i = 0; i < m; i++)
+        {
+            if (colour[i] == -1)
+            {
+                if (!colorGraph(i, m, graph, colour))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
