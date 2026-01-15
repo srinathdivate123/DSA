8dd83214347522daedc178833d7b0d73bfacf754
Srinath - ASUS <srinathdivate123@gmail.com>
2026-01-15 17:16:26 +0530

    S_G48_No_Of_Provinces_Using_DisJoinSet

--- Graphs/S_G47_Kruskals_Algorithm.cpp
+++ Graphs/S_G47_Kruskals_Algorithm.cpp
@@ -1,5 +1,9 @@
 // https://takeuforward.org/data-structure/kruskals-algorithm-minimum-spanning-tree-g-47ddd
 
+// TC: O(N + E) + O(E log E) + O(E * 4α * 2), where N = number of nodes and E = number of edges. O(N + E) for extracting edge information from the adjacency list. O(E log E) for sorting the array consisting of the edge tuples. Finally, we are using disjoint set operations inside a loop. The loop will continue for E times. Inside that loop, there are two disjoint set operations like findUPar() and UnionBySize(), each taking 4, so it will result in 4 * 2. That is why the last term O(E * 4 * 2) is added.
+
+// SC: O(N) + O(N) + O(E) , where E = number of edges and N = number of nodes. O(E) space is taken by the array that we are using to store the edge information. In the disjoint set data structure, we are using two N-sized arrays (i.e., a parent and a size array, as we are using unionBySize() function; otherwise, a rank array of the same size if unionByRank() is used), which result in the first two terms O(N).
+
 #include <bits/stdc++.h>
 using namespace std;
 
---
+++ Graphs/S_G48_No_Of_Provinces_Using_DisJoinSet.cpp
@@ -0,0 +1,109 @@
+// https://leetcode.com/problems/number-of-provinces/
+
+// This problem was earlier solved in (S_G7) using DFS. Here we'll solve it using DisJointSet. Unfortunately there is no TUF blog for this.
+
+// The intuition is that there wil be as many provinces as many unique ultimateParents are there. Hence you need to find the number of unique ultimateParents
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    // Function to find ultimate parent
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // Function to implement union by rank
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    // Function to implement union by size
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution
+{
+public:
+    int numProvinces(vector<vector<int>> adj, int V)
+    {
+        DisjointSet ds(V);
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (adj[i][j] == 1)
+                {
+                    ds.unionBySize(i, j);
+                }
+            }
+        }
+
+        int cnt = 0;
+        // Find the nodes who are themselves their parents (these are ultimateParents)
+        for (int i = 0; i < V; i++)
+        {
+            if (ds.parent[i] == i)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+int main()
+{
+
+    return 0;
+}
