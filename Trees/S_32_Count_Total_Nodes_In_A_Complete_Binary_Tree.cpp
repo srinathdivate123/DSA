2ea76d06e7c4af7d9c7a66cd1c2a8552bc11ec18
Srinath - ASUS <srinathdivate123@gmail.com>
2026-05-12 21:43:52 +0530

    S_32_Count_Total_Nodes_In_A_Complete_Binary_Tree S_34_Construct_A_Binary_Tree_From_Preorder_And_Inorder_Traversal

--- Trees/S_32_Count_Total_Nodes_In_A_Complete_Binary_Tree.cpp
+++ Trees/S_32_Count_Total_Nodes_In_A_Complete_Binary_Tree.cpp
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/count-complete-tree-nodes/description
 
 // Given the root of a complete binary tree, return the number of the nodes in the tree.
-// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h. Design an algorithm that runs in less than O(n) time complexity.
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2^h nodes inclusive at the last level h. Design an algorithm that runs in less than O(n) time complexity.
 
 // Brute force is to traverse the tree using inorder traversal and count the number of nodes as we are traversing the tree. But we will need to visit every node in the tree leading to O(n) time complexity.
 
@@ -10,11 +10,11 @@
 
 // We can exploit its properties of a complete binary tree to optimise the algorithm and achieve a better time complexity.
 // In a complete binary tree, the last level may not be completely filled, but the nodes are positioned from left to right. This property allows us to determine the number of nodes using just the height.
-// The relationship between the height of the binary tree (h) and the maximum number of nodes it can have, denoted by the formula:
+// The relationship between the height of the binary tree (h) and the maximum number of nodes it can have, is denoted by the formula:
 // Maximum Number of Nodes: 2^h-1
-// If the last level of a binary tree is perfectly filled, known as a perfect binary tree, the count of nodes can be determined by the formula: 2^h-1, where h is the height. To check if the last level of the Binary Tree is filled or not we can compare the left and right heights of the tree.
-// If the left height equals right height, it indicates that the last level is completely filled. Or else the last level is not completely filled.
-// When left and right height differ, we can employ a recursive approach. We recursively calculate the number of nodes in the left subtree and in the right subtree, and then return the total count as 1 + leftNodes + rightNodes.
+// If the last level of a binary tree is perfectly filled, known as a perfect binary tree, then nodes=2^h-1. To check if the last level is filled, we can compare the left and right heights of the tree.
+// If the left height equals right height, it indicates that the last level is completely filled.
+// When left and right height differ, we recursively calculate the number of nodes in the left subtree and in the right subtree, and then return the total count as 1 + leftNodes + rightNodes.
 
 // TC: O(log N * log N) The calculation of leftHeight and rightHeight takes O(log N) time.In the worst case, when encountering the second case (leftHeight != rightHeight), the recursive calls are made at most log N times (the height of the tree) Therefore, the total time complexity is O(log N * log N).
 
@@ -46,7 +46,7 @@ public:
         // If heights are equal, it's a perfect binary tree
         if (lh == rh)
         {
-            // Use formula: 2^h - 1
+            // The number of nodes in the below subtre: 2^h - 1
             return (1 << lh) - 1;
         }
         // Otherwise, recursively count left and right subtrees
---
+++ Trees/S_34_Construct_A_Binary_Tree_From_Preorder_And_Inorder_Traversal.cpp
@@ -0,0 +1,86 @@
+// https://takeuforward.org/data-structure/construct-a-binary-tree-from-inorder-and-preorder-traversal
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(N), as ach node is visited once.
+// Space Complexity: O(N), for the hashmap and recursion stack (worst case when tree is skewed).
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        // Map to store the index of each value in inorder
+        map<int, int> inorderMap;
+
+        // Fill the map with inorder values and their indices
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            inorderMap[inorder[i]] = i;
+        }
+
+        // Call the recursive helper function
+        return build(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inorderMap);
+    }
+
+private:
+    // Recursive function to build tree using preorder and inorder segments
+    TreeNode *build(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, map<int, int> &inorderMap)
+    {
+        if (preStart > preEnd || inStart > inEnd)
+        {
+            return nullptr;
+        }
+
+        // The first element in preorder is root
+        TreeNode *root = new TreeNode(preorder[preStart]);
+
+        // Find the root index in inorder
+        int inRoot = inorderMap[root->val];
+
+        // Number of elements in left subtree
+        int numsLeft = inRoot - inStart;
+
+        // Recursively build left and right subtrees
+        root->left = build(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inorderMap);
+        root->right = build(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inorderMap);
+
+        return root;
+    }
+};
+
+// Inorder traversal to print tree
+void printInorder(TreeNode *root)
+{
+    if (!root)
+        return;
+    printInorder(root->left);
+    cout << root->val << " ";
+    printInorder(root->right);
+}
+
+int main()
+{
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    vector<int> preorder = {3, 9, 20, 15, 7};
+
+    Solution sol;
+    TreeNode *root = sol.buildTree(preorder, inorder);
+
+    cout << "Inorder of Unique Binary Tree Created:\n";
+    printInorder(root);
+    cout << endl;
+
+    return 0;
+}
